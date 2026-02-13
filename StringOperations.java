197ca42770125759b6b49dc1e18d7d89206c1ac9
KOTTEESWARAN2008 <ramusvc957@gmail.com>
2026-02-13 15:55:36 +0530

    Add files via upload

--- StringOperations.java
+++ StringOperations.java
@@ -1,23 +1,26 @@
+
+textops.StringOperations.java
 package textops;
-public class StringOperations{
-private String=str1;
-private String=str2;
-public StringOperations(String str1,String str2){
-this.str=str1;
-this.str=str2;
+public class StringOperations {
+private String str1;
+private String str2;
+public StringOperations(String str1, String str2) {
+this.str1 = str1;
+this.str2 = str2;
 }
-public void exploreStringMethods(){
-System.out.println("----String Methods----");
-System.out.println("Length of str1:"+str.1length());
-System.out.println("Uppercase str1:"+str.toUpperCase());
-System.out.println("Lowercase str1:"+str.toLowerCase());
-System.out.println("Concatenation:"+str.toconcat(str2));
-System.out.println("Equals:"str1.equals(str));
-System.out.println("Equals IgnoreCase:"+str.equalsIgnoreCase(str2));
-System.out.println("Contains str2 in str1:"+str.contains(str2));
-System.out.println("substring of str(0,3):"+(str.length()>=3?str.substring(0,3):"Too short"));
-System.out.println("Replace 'a' with 'x' str1:"+str.replace('a','x'));
-System.out.println("Index of str2 in str1:"+str1.indexOf(str2);
-System.out.println("Trim str:"+str1.trim()+"'");
+public void exploreStringMethods() {
+System.out.println("----- String Methods -----");
+System.out.println("Length of str1: " + str1.length());
+System.out.println("Uppercase str1: " + str1.toUpperCase());
+System.out.println("Lowercase str1: " + str1.toLowerCase());
+System.out.println("Concatenation: " + str1.concat(str2));
+System.out.println("Equals: " + str1.equals(str2));
+System.out.println("Equals Ignore Case: " + str1.equalsIgnoreCase(str2));
+System.out.println("Contains str2 in str1: " + str1.contains(str2));
+System.out.println("Substring of str1 (0,3): " +
+(str1.length() >= 3 ? str1.substring(0, 3) : "Too short"));
+System.out.println("Replace 'a' with 'x' in str1: " + str1.replace('a', 'x'));
+System.out.println("Index of str2 in str1: " + str1.indexOf(str2));
+System.out.println("Trim str1: '" + str1.trim() + "'");
 }
 }
